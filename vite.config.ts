120919d32829e7b3148b7daab490a44b9fdd9f7a
test1 <test>
2024-05-27 17:54:02 +0900

    good4

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/CNU_Blog/',
+  base: '2024-realcoding/CNU_Blog/',
   plugins: [react()],
 });
